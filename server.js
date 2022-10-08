9b6fa9997063b7695948f2c1e283818893c8420b
homing1923 <homing1923@gmail.com>
2022-10-08 15:32:42 -0400

    Co-authored-by: karanmeghani18 <karanmeghani18@users.noreply.github.com>

--- server.js
+++ server.js
@@ -18,11 +18,12 @@ app.set('view engine', '.hbs');
 mg.connect("mongodb+srv://dbUser:dbPassword@t440cluster.uwdgov5.mongodb.net/?retryWrites=true&w=majority");
 
 //Server Const
-const Userschema = new mg.Schema({id:Number,username:String,password:String,email:String,name:String,cart:Array,Membership:Boolean});
+const userschema = new mg.Schema({id:Number,username:String,password:String,email:String,name:String,cart:Array,Membership:Boolean});
+const lessonschema = new mg.Schema({name:String,instructor:String,duration:Number,fee:Number});
+ 
+const lessons = mg.model("lessons", lessonschema);
+const users = mg.model("users", userschema);
 
-const lessonschema = new mg.Schema({id:Number,name:String,instructor:String,duration:Number,fee:Number});
-
-// const lessons = new lessonschema({id:,name,duration}); new lesson creations 
 
 
 //Default listener
@@ -37,8 +38,29 @@ app.get("/", (req, res) => {
 });
 
 app.get("/class", (req, res) => {
-    res.render("timetable", {layout:"mainframe"})
+    lessons.find({}).lean().exec()
+    .then(response =>{
+        res.status(200).render("timetable",{layout:'mainframe', data:response});
+        console.log(response);
+    })
+    .catch(err =>{
+        res.status(500).render("timetable",{layout:'mainframe', err:err});
+    })
+    // res.render("timetable", {layout:"mainframe"})
 });
+
+app.post("/createclass", (req,res) =>{
+    const newlesson = new lessons({name:req.body.nameinput, instructor:req.body.instructor, duration:req.body.durationinput, fee:req.body.fee})
+    newlesson.save()
+    .then(response =>{
+        // if(response)
+        console.log(reponse);
+        res.status(200).render("",{layout:"mainframe"});
+    })
+    .catch(err =>{
+        res.status(500).render("lessonmanage",{layout:"mainframe"});
+    })
+})
 
 app.get("/login", (req, res) => {
     res.render("login", {layout:"mainframe"})
@@ -46,6 +68,9 @@ app.get("/login", (req, res) => {
 
 app.get("/signup", (req, res) => {
     res.render("signup", {layout:"mainframe"})
+});
+app.get("/cart", (req, res) => {
+    res.render("cart", {layout:"mainframe"})
 });
 
 app.post("/login", (req, res) => {
