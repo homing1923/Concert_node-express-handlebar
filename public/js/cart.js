f250698850cc29861e0691ac777d2a50d5dcc907
Karan <karanmeghani18@gmail.com>
2022-10-12 17:55:25 -0400

    c

--- public/js/cart.js
+++ public/js/cart.js
@@ -8,11 +8,11 @@ const membership = (value) =>{
     }
 } 
     
-const durations = document.getElementsByClassName("duration").value;
+const durations = document.getElementById("duration")[0].value;
 // document.getElementById("testing").innerText(`${durations}`);
 const forms= document.getElementsByTagName("form")
 let totalduration=0
-for(i=o;i<forms.length;i++){
+for(i=0;i<forms.length;i++){
     totalduration+=Number(forms[i].getElementsByTagName("input").value);
 
 }
@@ -20,6 +20,6 @@ let subtotal = totalduration*0.58
 let tax= subtotal*0.13
 let total= subtotal+tax
 
-document.getElementById("calculation").innerHTML(`<p> Subtotal: ${subtotal} </p>
+document.getElementById("calculation").innerHTML()=(`<p> Subtotal: ${subtotal} </p>
                                                 <p>Tax: ${tax}</p>
-                                                <p>Total: ${total}`)
+                                                <p>Total: ${total}</p>`)
