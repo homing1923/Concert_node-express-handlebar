d9116ba65e20ea2cdccf6171d4153d9b28e46fa7
homing1923@Window <homing1923@gmail.com>
2022-10-13 00:31:22 -0400

    class.js error handling

--- public/js/class.js
+++ public/js/class.js
@@ -9,20 +9,20 @@ const checkregistered = () =>{
     if(usercartitems.length > 0){
         for(each in usercartitems){
             let currentrow = document.getElementById(`${usercartitems[each].value}`);
-            let rowbtn;
-            try {
-                rowbtn = currentrow.getElementsByTagName("button")[0];
-                rowbtn.setAttribute("class", "lessonregisteredbtn");
-                rowbtn.setAttribute("type", "button");
-                document.getElementById(usercartitems[each].value).innerHTML +=
-                `
-                <br>
-                <p class="lessonregisteredbar">This item is already in your cart</p>
-                `;
+            if (currentrow !== null){
+                let rowbtn = currentrow.getElementsByTagName("button");
+                if(rowbtn.length > 0){
+                    rowbtn = rowbtn[0];
+                    rowbtn.setAttribute("class", "lessonregisteredbtn");
+                    rowbtn.setAttribute("type", "button");
+                    document.getElementById(usercartitems[each].value).innerHTML +=
+                    `
+                    <br>
+                    <p class="lessonregisteredbar">This item is already in your cart</p>
+                    `;
+                }
+            }
 
-            } catch (error) {
-                console.log(error);
-            }   
         }
     }
 }
